5a10953fc2ad4d5f03a30ef340bd69704b9404b3
J-E Castagnede <j.e.castagnede@gmail.com>
2019-07-24 10:19:35 +0200

    fux urls

--- terra_crud/urls.py
+++ terra_crud/urls.py
@@ -9,6 +9,6 @@ router = SimpleRouter()
 router.register('groups', views.CrudGroupViewSet)
 router.register('views', views.CrudViewViewSet)
 
-urls_patterns = [
+urlpatterns = [
     path('api/crud/', include(router.urls)),
 ]
