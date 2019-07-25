bf300f3d6fe033f0959de7c39383a47c2211aee5
J-E Castagnede <j.e.castagnede@gmail.com>
2019-07-25 20:05:38 +0200

    add command to generate views

---
+++ terra_crud/management/commands/create_default_crud_views.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from terracommon.terra.models import Layer
+
+from ...models import CrudView
+
+
+class Command(BaseCommand):
+    help = 'Create view for existing layers'
+
+    def handle(self, *args, **options):
+        for layer in Layer.objects.all():
+            CrudView.objects.get_or_create(layer=layer,
+                                           defaults={"name": layer.name, "order": 0})
---
+++ terra_crud/tests/test_commands.py
@@ -0,0 +1,24 @@
+from django.core.management import call_command
+from django.test.testcases import TestCase
+from terracommon.terra import GeometryTypes
+from terracommon.terra.models import Layer
+
+from terra_crud.models import CrudView
+
+
+class CreateDefaultCrudViewTestCase(TestCase):
+    def setUp(self):
+        self.layer_1 = Layer.objects.create(name='1', geom_type=GeometryTypes.Point)
+        self.layer_2 = Layer.objects.create(name='2', geom_type=GeometryTypes.LineString)
+
+    def test_views_created(self):
+        call_command('create_default_crud_views')
+
+        self.assertTrue(CrudView.objects.filter(layer__pk__in=[self.layer_1.pk, self.layer_2.pk]).exists())
+
+    def test_views_not_created_if_already_exists(self):
+        layer_3 = Layer.objects.create(name='3', geom_type=GeometryTypes.Polygon)
+        CrudView.objects.create(name=layer_3.name, layer=layer_3, order=0)
+
+        call_command('create_default_crud_views')
+        self.assertEqual(CrudView.objects.count(), 3)
