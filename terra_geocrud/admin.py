5962bd015fdbc34400a8ae5d6d1cab1179cdfda4
J-E Castagnede <j.e.castagnede@gmail.com>
2019-10-08 12:25:46 +0200

    update admin

--- terra_geocrud/admin.py
+++ terra_geocrud/admin.py
@@ -26,7 +26,7 @@ class CrudViewAdmin(admin.ModelAdmin):
     readonly_fields = ['form_schema', 'properties']
     fieldsets = (
         (None, {'fields': (('name', 'layer'), ('group', 'order', 'pictogram'))}),
-        ('Feature list', {'fields': ('properties', 'default_list_properties')}),
+        ('Feature properties', {'fields': ('properties', 'default_list_properties', 'feature_title_property')}),
         ("Document generation", {'fields': ('templates', )}),
         ("Schema", {'fields': ('ui_schema', )}),
         ("Other settings", {'fields': (('map_style', 'settings'), )}),
