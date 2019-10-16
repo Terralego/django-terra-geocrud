3c1be3e8b58935db5398e8c8e5fcb9f65a0ef7fa
J-E Castagnede <j.e.castagnede@gmail.com>
2019-10-16 17:25:11 +0200

    fix reversion

--- test_terra_geocrud/settings/__init__.py
+++ test_terra_geocrud/settings/__init__.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_json_widget',
     'template_model',
     'template_engines',
+    'reversion',
     'geostore',
     'terra_geocrud',
     'test_terra_geocrud.test_app'
