3aa13c421a63970a6d689383add8e6aaa10f28ae
J-E Castagnede <j.e.castagnede@gmail.com>
2019-10-16 11:03:33 +0200

    update to work with geostore 0.3.9

--- setup.py
+++ setup.py
@@ -44,10 +44,10 @@ setup(
         'django-reversion>=3.0.4',
         'django-template-model>=1.0.1',
         'django-template-engines>=1.2.9',
-        'django-geostore>=0.3.8',
+        'django-geostore>=0.3.9',
         'djangorestframework>=3.10',
         'djangorestframework-gis>=0.14'
-        'django>=2.2,<3.0',
+        'django>=2.2',
         'django-json-widget>=0.2.0',
     ],
     tests_require=tests_require,
