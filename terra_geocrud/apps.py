383e577e920b3a1675e03fabef074595509a2926
J-E Castagnede <j.e.castagnede@gmail.com>
2019-11-05 17:36:12 +0100

    use accounts and new permissions

--- terra_geocrud/apps.py
+++ terra_geocrud/apps.py
@@ -9,11 +9,11 @@ class TerraCrudConfig(PermissionRegistrationMixin, AppConfig):
     name = 'terra_geocrud'
     verbose_name = 'Geographic Editor Config'
     permissions = (
-        ("geocrud_can_manage_views", _("GEOCRUD: Can create / edit / delete views / groups and associated layers.")),
-        ("geocrud_can_view_feature", _("GEOCRUD: Can read feature detail.")),
-        ("geocrud_can_add_feature", _("GEOCRUD: Can create feature")),
-        ("geocrud_can_change_feature", _("GEOCRUD: Can change feature")),
-        ("geocrud_can_delete_feature", _("GEOCRUD: Can delete feature")),
+        ("can_manage_views", _("GEOCRUD: Can create / edit / delete views / groups and associated layers.")),
+        ("can_view_feature", _("GEOCRUD: Can read feature detail.")),
+        ("can_add_feature", _("GEOCRUD: Can create feature")),
+        ("can_change_feature", _("GEOCRUD: Can change feature")),
+        ("can_delete_feature", _("GEOCRUD: Can delete feature")),
     )
 
     def ready(self):
