680b242d90d79b3ee6e88b7b4b7698cf2f50ef7d
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-11-25 16:44:16 +0100

    Get style from MapBaseLAyer

--- terra_geocrud/utils.py
+++ terra_geocrud/utils.py
@@ -1,5 +1,14 @@
 from . import settings as app_settings
 
+DEFAULT_MBGL_RENDERER_STYLE = {'version': 8,
+                               'sources': {"DEFAULT_MBGL_RENDERER_STYLE": {"type": "raster",
+                                           "tiles": ["http://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+                                           "tileSize": 256,
+                                           "maxzoom": 18}},
+                               'layers': [{'id': 'DEFAULT_MBGL_RENDERER_STYLE',
+                                           "type": "raster",
+                                           "source": "DEFAULT_MBGL_RENDERER_STYLE"}]}
+
 
 def get_default_style(layer):
     style_settings = app_settings.TERRA_GEOCRUD.get('STYLES', {})
