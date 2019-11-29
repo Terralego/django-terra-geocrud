9d814dc8fe23489729c84b6d7ff8d459ed0b8c94
J-E Castagnede <j.e.castagnede@gmail.com>
2019-11-29 14:37:43 +0100

    fix requirements

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'django-template-model>=1.0.1',
         'django-template-engines>=1.2.22',
         'django-mapbox-baselayer>=0.0.2',
-        'django-geostore@https://github.com/Terralego/django-geostore/archive/relations.tar.gz',
+        'django-geostore@https://github.com/Terralego/django-geostore/archive/extra_features.tar.gz',
         'django-terra-accounts>=0.3.7.1',
         'djangorestframework>=3.10',
         'djangorestframework-gis>=0.14'
