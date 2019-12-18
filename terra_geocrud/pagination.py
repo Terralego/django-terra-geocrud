f0724a5538932ac2943c22a56fb776df36e030b1
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-18 10:45:40 +0100

    fix pagination

--- terra_geocrud/pagination.py
+++ terra_geocrud/pagination.py
@@ -12,6 +12,6 @@ class GeoJsonPagination(PagePagination):
                 ('count', self.page.paginator.count),
                 ('next', self.get_next_link()),
                 ('previous', self.get_previous_link()),
-                ('features', data['results'])
+                ('features', data)
             ]))
         return super().get_paginated_response(data)
