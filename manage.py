80d019d3c74c2dac1895f433f9033c8e7e789128
J-E Castagnede <j.e.castagnede@gmail.com>
2020-02-04 10:38:54 +0100

    use venv by default

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
+#!/bin/bash
+"exec" "`dirname $0`/../venv/bin/python" "$0" "$@"  # NOQA
 
+import os
 import sys
 
 
