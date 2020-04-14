9979c42596245e6aeb50a12274e4c5b9ed0ef6f4
J-E Castagnede <j.e.castagnede@gmail.com>
2020-04-14 14:13:18 +0200

    give allowed image extensions and max file size to fontend through API settings

--- terra_geocrud/settings.py
+++ terra_geocrud/settings.py
@@ -1,11 +1,17 @@
 from copy import deepcopy
 
+from PIL import Image
 from django.conf import settings
 
+Image.init()
 _DEFAULT_TERRA_GEOCRUD = {
     # default extent to world
     'EXTENT': [-90.0, -180.0, 90.0, 180.0],
     'DATA_FILE_STORAGE_CLASS': 'django.core.files.storage.FileSystemStorage',
+    # allowed image extensions are same as PIL support by default
+    'IMAGE_EXTENSION_ALLOWED': [ext for ext in Image.EXTENSION],
+    # give max_memory_size allowed by backend to api settings
+    'FILE_UPLOAD_MAX_MEMORY_SIZE': settings.FILE_UPLOAD_MAX_MEMORY_SIZE,
     # Do not finish the url with a slash
     'MBGLRENDERER_URL': 'http://mbglrenderer',
     # We should automatically get the source of layers from a model
