e46a03eab5081af161f30f9a60a1d246320d2015
J-E Castagnede <j.e.castagnede@gmail.com>
2020-04-14 10:49:15 +0200

    fix test

--- terra_geocrud/tests/test_models.py
+++ terra_geocrud/tests/test_models.py
@@ -7,9 +7,10 @@ from terra_geocrud.tests.factories import CrudViewFactory, FeaturePictureFactory
 
 from geostore.models import Feature
 from terra_geocrud.models import AttachmentCategory, AttachmentMixin, \
-    feature_attachment_directory_path, feature_picture_directory_path
+    feature_attachment_directory_path, feature_picture_directory_path, CrudViewProperty
 from terra_geocrud.tests import factories
 from .. import models
+from ..properties.schema import sync_layer_schema, sync_ui_schema
 
 
 class CrudModelMixinTestCase(TestCase):
@@ -55,9 +56,68 @@ class FeaturePropertyDisplayGroupTestCase(TestCase):
     def setUp(self) -> None:
         self.crud_view = factories.CrudViewFactory()
         self.group_1 = models.FeaturePropertyDisplayGroup.objects.create(crud_view=self.crud_view, label='test')
+        self.group_2 = models.FeaturePropertyDisplayGroup.objects.create(crud_view=self.crud_view, label='test2')
+        CrudViewProperty.objects.create(view=self.crud_view, key="test",
+                                        group=self.group_1,
+                                        required=True,
+                                        json_schema={'type': "object", "title": "test"}
+                                        )
+        CrudViewProperty.objects.create(view=self.crud_view, key="name",
+                                        group=self.group_2,
+                                        required=True,
+                                        json_schema={'type': "string", "title": "Name"},
+                                        ui_schema={'ui:widget': 'textarea'})
+        CrudViewProperty.objects.create(view=self.crud_view, key="age",
+                                        group=self.group_1,
+                                        json_schema={'type': "integer", "title": "Age"})
+        CrudViewProperty.objects.create(view=self.crud_view, key="country",
+                                        json_schema={'type': "string", "title": "Country"})
+        self.feature = Feature.objects.create(geom=Point(0, 0, srid=4326),
+                                              properties={"age": 10, "name": "jec", "country": "slovenija"},
+                                              layer=self.crud_view.layer)
+        self.template = factories.TemplateDocxFactory()
+        self.crud_view.templates.add(self.template)
+        sync_layer_schema(self.crud_view)
+        sync_ui_schema(self.crud_view)
 
     def test_str(self):
         self.assertEqual(str(self.group_1), self.group_1.label)
+
+    def test_form_schema(self):
+        self.assertDictEqual(self.group_1.form_schema,
+                             {'properties': {'age': {'title': 'Age', 'type': 'integer'}},
+                              'required': [],
+                              'title': 'test',
+                              'type': 'object'
+                              })
+        self.assertDictEqual(self.group_2.form_schema,
+                             {'properties': {'name': {'title': 'Name', 'type': 'string'}},
+                              'required': ['name'],
+                              'title': 'test2',
+                              'type': 'object'
+                              })
+        self.maxDiff = None
+        self.assertDictEqual(self.crud_view.grouped_form_schema, {
+            "required": [],
+            "properties": {
+                'country': {
+                    'type': 'string',
+                    'title': 'Country'
+                },
+                'test': {
+                    'properties': {'age': {'title': 'Age', 'type': 'integer'}},
+                    'required': [],
+                    'title': 'test',
+                    'type': 'object'
+                },
+                'test2': {
+                    'properties': {'name': {'title': 'Name', 'type': 'string'}},
+                    'required': ['name'],
+                    'title': 'test2',
+                    'type': 'object'
+                },
+            }
+        })
 
 
 class AttachmentCategoryTestCase(TestCase):
