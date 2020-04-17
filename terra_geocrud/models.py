f087b6fb36652992549ba5fa5925c6c003f33262
J-E Castagnede <j.e.castagnede@gmail.com>
2020-04-17 10:59:32 +0200

    fix title

--- terra_geocrud/models.py
+++ terra_geocrud/models.py
@@ -100,7 +100,7 @@ class CrudView(FormSchemaMixin, MapStyleModelMixin, CrudModelMixin):
 
     def get_feature_title(self, feature):
         """ Get feature title base on title field. Return identifier if empty or None """
-        data = feature.properties.get(self.feature_title_property, '')\
+        data = feature.properties.get(self.feature_title_property.key, '')\
             if self.feature_title_property else feature.identifier
         return data or feature.identifier
 
