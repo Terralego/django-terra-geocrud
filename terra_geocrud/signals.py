97a6ecc92ca43d52c29b6f521a7ecb0a1c46e320
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2021-03-10 10:30:24 +0100

    Change update_fields list

--- terra_geocrud/signals.py
+++ terra_geocrud/signals.py
@@ -17,4 +17,6 @@ def save_feature(sender, instance, **kwargs):
     if app_settings.GEOSTORE_RELATION_CELERY_ASYNC:
         kwargs['relation_id'] = None
         kwargs.pop('signal')
+        update_fields = kwargs.get('update_fields')
+        kwargs['update_fields'] = list(update_fields) if update_fields else update_fields
         execute_async_func(feature_update_relations_destinations, (instance.pk, kwargs))
