9620a2d791573d41a587792a3c07888d9f366a4b
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2021-03-10 21:29:01 +0100

    Fix code climate

--- test_terra_geocrud/functions_test.py
+++ test_terra_geocrud/functions_test.py
@@ -15,13 +15,15 @@ def get_cities(feature):
         return []
 
 
-def get_first_city(feature):
-    start_point = Point(feature.geom[0]).ewkt
+def get_city(feature, id_point):
+    start_point = Point(feature.geom[id_point]).ewkt
     lr = LayerRelation.objects.get(name='cities')
     return ', '.join(feature.get_stored_relation_qs(lr.pk).intersects(start_point).exclude(properties__name=None).values_list('properties__name', flat=True))
 
 
+def get_first_city(feature):
+    return get_city(feature, 0)
+
+
 def get_last_city(feature):
-    last_point = Point(feature.geom[-1]).ewkt
-    lr = LayerRelation.objects.get(name='cities')
-    return ', '.join(feature.get_stored_relation_qs(lr.pk).intersects(last_point).exclude(properties__name=None).values_list('properties__name', flat=True))
+    return get_city(feature, -1)
