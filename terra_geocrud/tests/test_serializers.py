3caa8f6baf9abd66c4dc77236c67ed4524a687e4
J-E Castagnede <j.e.castagnede@gmail.com>
2021-03-12 12:33:21 +0100

    fix tests

--- terra_geocrud/tests/test_serializers.py
+++ terra_geocrud/tests/test_serializers.py
@@ -74,7 +74,7 @@ class CrudFeatureSerializer(TestCase):
                 "title": "Type",
             },
         )
-        PropertyEnum.objects.create(value='type_1', pictogram=ContentFile(SMALL_PICTURE))
+        PropertyEnum.objects.create(value='type_1', pictogram=ContentFile(SMALL_PICTURE), property=prop_with_values)
         self.feature = Feature.objects.create(geom='POINT(0 0)',
                                               properties={
                                                   "date_start": "test",
